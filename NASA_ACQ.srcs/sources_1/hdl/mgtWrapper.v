b2c371e7f1b46ef5b0ac73b76190b8528dbe5b8f
Eric Norum <wenorum@lbl.gov>
2024-03-06 16:22:39 -0800

    Try an even longer NULL run.  This problem seems intractable!

--- NASA_ACQ.srcs/sources_1/hdl/mgtWrapper.v
+++ NASA_ACQ.srcs/sources_1/hdl/mgtWrapper.v
@@ -781,7 +781,7 @@ module mgtLinkStatus #(
     output wire                       rxLinkUp);
 
 localparam COMMAS_REQUIRED = 128;
-localparam NULLS_REQUIRED = 32;
+localparam NULLS_REQUIRED  = 150;
 
 localparam COMMA_COUNTER_LOAD = COMMAS_REQUIRED - 1;
 localparam COMMA_COUNTER_WIDTH = $clog2(COMMAS_REQUIRED + 1) + 1;
