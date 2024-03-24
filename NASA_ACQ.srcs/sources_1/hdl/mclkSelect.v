00ff20c6daeb4b13014f47e4675f558ad788007d
Eric Norum <wenorum@lbl.gov>
2024-03-24 13:58:22 -0700

    Make MCLK changes glitch-free.  This adds a few more false clock constraints.

--- NASA_ACQ.srcs/sources_1/hdl/mclkSelect.v
+++ NASA_ACQ.srcs/sources_1/hdl/mclkSelect.v
@@ -23,11 +23,12 @@
  */
 
 /*
- * Select the approprate clock to send to the ADC MCLK
+ * Generate and select the approprate clock to send to the ADC MCLK
  */
 `default_nettype none
 module mclkSelect #(
-    parameter DEBUG = "false"
+    parameter SYSCLK_RATE = 100000000,
+    parameter DEBUG       = "false"
     ) (
     input  wire        sysClk,
     input  wire        sysCsrStrobe,
@@ -38,29 +39,78 @@ module mclkSelect #(
     (*MARK_DEBUG=DEBUG*) input  wire clk40p96,
     (*MARK_DEBUG=DEBUG*) input  wire clk51p2,
     (*MARK_DEBUG=DEBUG*) input  wire clk64,
-    (*MARK_DEBUG=DEBUG*) output wire MCLK_BUFG,
-    (*MARK_DEBUG=DEBUG*) output wire MCLK);
+    (*MARK_DEBUG=DEBUG*) output wire MCLK,
+                         output wire MCLK_BUFG);
 
-localparam MUXSEL_WIDTH = 2;
-(*MARK_DEBUG=DEBUG*) reg [MUXSEL_WIDTH-1:0] muxSel;
+localparam CLOCK_COUNT = 4;
+localparam SLOWEST_CLOCK = 32768000;
+localparam CLOCK_MUXSEL_WIDTH = $clog2(CLOCK_COUNT);
+
+/*
+ * Delay for at least 3 cycles of slowest clock.
+ * This ensures glitch-freee transitions between clocks.
+ */
+localparam DELAY_TICKS = (SYSCLK_RATE+(SLOWEST_CLOCK/3)-1)/(SLOWEST_CLOCK/3);
+localparam DELAY_LOAD = DELAY_TICKS - 2;
+localparam DELAY_COUNTER_WIDTH = $clog2(DELAY_LOAD+1) + 1;
+reg [DELAY_COUNTER_WIDTH-1:0] delayCounter = DELAY_LOAD;
+wire delayCounterDone = delayCounter [DELAY_COUNTER_WIDTH-1];
+
+(*MARK_DEBUG=DEBUG*) reg [CLOCK_COUNT-1:0] activeClock = 0, newClock = 0;
+
 always @(posedge sysClk) begin
     if (sysCsrStrobe) begin
-        muxSel <= sysGPIO_OUT[MUXSEL_WIDTH-1:0];
+        newClock <= 1 << sysGPIO_OUT[CLOCK_MUXSEL_WIDTH-1:0];
+        activeClock <= 0;
+        delayCounter <= DELAY_LOAD;
+    end
+    else if (!delayCounterDone) begin
+        delayCounter <= delayCounter - 1;
+    end
+    else begin
+        activeClock <= newClock;
     end
 end
-assign sysStatus = { {32-MUXSEL_WIDTH{1'b0}}, muxSel };
+assign sysStatus = { {32-CLOCK_COUNT{1'b0}}, activeClock };
 
-reg d16p384 = 0, d20p48 = 0, d25p6 = 0, d32 = 0;
-assign MCLK = (muxSel == 1) ? d25p6   :
-              (muxSel == 2) ? d20p48  :
-              (muxSel == 3) ? d16p384 :
-                              d32;
-always @(posedge clk32p768) begin d16p384 <= !d16p384; end
-always @(posedge clk40p96 ) begin d20p48  <= !d20p48 ; end
-always @(posedge clk51p2  ) begin d25p6   <= !d25p6  ; end
-always @(posedge clk64    ) begin d32     <= !d32    ; end
-
+wire clk16p384, clk20p48, clk25p6, clk32;
+assign MCLK = |{ clk16p384, clk20p48, clk25p6, clk32 };
 BUFG BUFG_MCLK (.I(MCLK), .O(MCLK_BUFG));
 
+mclkSelectClockGen mclkSelectClockGen32 (
+    .clkIn(clk64),
+    .en_a(activeClock[0]),
+    .clkOut(clk32));
+mclkSelectClockGen mclkSelectClockGen25p6 (
+    .clkIn(clk51p2),
+    .en_a(activeClock[1]),
+    .clkOut(clk25p6));
+mclkSelectClockGen mclkSelectClockGen20p48 (
+    .clkIn(clk40p96),
+    .en_a(activeClock[2]),
+    .clkOut(clk20p48));
+mclkSelectClockGen mclkSelectClockGen16p384 (
+    .clkIn(clk32p768),
+    .en_a(activeClock[3]),
+    .clkOut(clk16p384));
+endmodule
+
+module mclkSelectClockGen (
+    input  wire clkIn,
+    input  wire en_a,
+    output reg  clkOut = 0);
+
+(*ASYNC_REG="true"*) reg en_m;
+reg en;
+always @(posedge clkIn) begin
+    en_m <= en_a;
+    en   <= en_m;
+    if (en) begin
+        clkOut <= ~clkOut;
+    end
+    else begin
+        clkOut <= 0;
+    end
+end
 endmodule
 `default_nettype wire
