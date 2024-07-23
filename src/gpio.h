9a1d7343f28f0c331d6b1736c2b0b72e14213090
Eric Norum <enorum@ospreydcs.com>
2024-07-23 08:05:47 -0700

    Count ADC starts using new rising edge counter block.  Pulses as short as 5 ns will be counted.

--- src/gpio.h
+++ src/gpio.h
@@ -72,6 +72,7 @@
 #define GPIO_IDX_MCLK_CLK32P00_ERROR_COUNT  41
 #define GPIO_IDX_MCLK_CLK25P60_ERROR_COUNT  42
 #define GPIO_IDX_MCLK_CLK20P48_ERROR_COUNT  43
+#define GPIO_IDX_ADC_STARTCOUNT             44
 
 #define GPIO_IDX_COUNT                     64 // Number of GPIO registers
 
