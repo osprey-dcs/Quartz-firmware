ba011d3ba97a3ad530db6e1da5231121cced43b6
Eric Norum <enorum@ospreydcs.com>
2024-07-24 11:09:48 -0700

    Make AD7768 DCLK/DRDY recorder more useful.  Trigger on DRDY misalignment.  Record both pre and post trigger values.

--- NASA_ACQ.srcs/sources_1/hdl/ad7768recorder.v
+++ NASA_ACQ.srcs/sources_1/hdl/ad7768recorder.v
@@ -24,9 +24,7 @@
 
 /*
  * Simple logic analyzer recording of AD7768 DCLK and DRDY lines.
- * Trigger is rising edge of first DRDY line or lines.
- * No pretrigger samples, but we know that the sample before the first must
- * have all DRDY lines low
+ * Trigger on misalignment of DRDY lines.
  */
 `default_nettype none
 module ad7768recorder #(
@@ -44,6 +42,8 @@ module ad7768recorder #(
     input  wire [ADC_CHIP_COUNT-1:0] adcDRDY_a);
 
 localparam ADDRESS_WIDTH = $clog2(SAMPLE_COUNT);
+localparam POST_TRIGGER_COUNT = SAMPLE_COUNT / 8;
+localparam TRIGGER_THRESHOLD = 3;
 
 //
 // Simple dual-port RAM
@@ -54,7 +54,7 @@ reg [(2*ADC_CHIP_COUNT)-1:0] dpram [0:SAMPLE_COUNT-1];
 // System clock (sysClk) domain
 ///////////////////////////////////////////////////////////////////////////////
 
-reg sysStartAcquisitionToggle = 0;
+reg sysArmToggle = 0;
 reg [ADDRESS_WIDTH-1:0] sysReadAddress;
 reg [(2*ADC_CHIP_COUNT)-1:0] sysReadQ;
 
@@ -62,7 +62,7 @@ always @(posedge sysClk) begin
     sysReadQ <= dpram[sysReadAddress];
     if (sysCsrStrobe) begin
         if (sysGPIO_OUT[31]) begin
-            sysStartAcquisitionToggle <= !sysStartAcquisitionToggle;
+            sysArmToggle <= !sysArmToggle;
         end
         sysReadAddress <= sysGPIO_OUT[ADDRESS_WIDTH-1:0];
     end
@@ -72,7 +72,6 @@ end
 // Acquisition clock (acqClk) domain
 ///////////////////////////////////////////////////////////////////////////////
 
-///////////////////////////////////////////////////////////////////////////////
 // Get asynchronous DCLK and DRDY into acquisition clock domain.
 // Use delayed synchronized value to sample first DRDY and all data lines.
 (*ASYNC_REG="true"*) reg [ADC_CHIP_COUNT-1:0] dclk_m = 0, drdy_m = 0;
@@ -86,49 +85,77 @@ always @(posedge acqClk) begin
 end
 
 // Waveform recorder
-localparam [2:0] ST_STATE_IDLE         = 2'd0,
-                 ST_STATE_AWAIT_LOW    = 2'd1,
-                 ST_STATE_AWAIT_RISING = 2'd2,
-                 ST_STATE_ACTIVE       = 2'd3;
-(*MARK_DEBUG=DEBUG*) reg [1:0] acqState = ST_STATE_IDLE;
+localparam [2:0] ST_IDLE          = 2'd0,
+                 ST_FILL          = 2'd1,
+                 ST_AWAIT_TRIGGER = 2'd2,
+                 ST_POST_TRIGGER  = 2'd3;
+(*MARK_DEBUG=DEBUG*) reg [1:0] acqState = ST_IDLE;
 reg acqAcquisitionActive = 0;
 reg [ADDRESS_WIDTH-1:0] acqWriteAddress = 0;
 
-(*ASYNC_REG="true"*) reg [ADC_CHIP_COUNT-1:0] acqStartAcquisitionToggle_m = 0;
-reg acqStartAcquisitionToggle = 0, acqStartAcquisitionMatch = 0;
+localparam PRE_TRIGGER_LOAD = (SAMPLE_COUNT - POST_TRIGGER_COUNT) - 2;
+localparam PRE_TRIGGER_COUNTER_WIDTH = $clog2(PRE_TRIGGER_LOAD+1) + 1;
+(*MARK_DEBUG=DEBUG*)
+reg [PRE_TRIGGER_COUNTER_WIDTH-1:0] preTriggerCounter = PRE_TRIGGER_LOAD;
+wire preTriggerDone = preTriggerCounter[PRE_TRIGGER_COUNTER_WIDTH-1];
+
+localparam POST_TRIGGER_LOAD = (POST_TRIGGER_COUNT - TRIGGER_THRESHOLD) - 2;
+localparam POST_TRIGGER_COUNTER_WIDTH = $clog2(POST_TRIGGER_LOAD+1) + 1;
+(*MARK_DEBUG=DEBUG*)
+reg [POST_TRIGGER_COUNTER_WIDTH-1:0] postTriggerCounter = POST_TRIGGER_LOAD;
+wire postTriggerDone = postTriggerCounter[POST_TRIGGER_COUNTER_WIDTH-1];
+
+localparam MISALIGN_COUNT_LOAD = TRIGGER_THRESHOLD - 2;
+localparam MISALIGN_COUNTER_WIDTH = $clog2(MISALIGN_COUNT_LOAD+1) + 1;
+(*MARK_DEBUG=DEBUG*)
+reg [MISALIGN_COUNTER_WIDTH-1:0] misalignCount = MISALIGN_COUNT_LOAD;
+wire misaligned = misalignCount[MISALIGN_COUNTER_WIDTH-1];
+
+(*ASYNC_REG="true"*) reg [ADC_CHIP_COUNT-1:0] acqArmToggle_m = 0;
+(*MARK_DEBUG=DEBUG*) reg acqArmToggle = 0, acqArmToggle_d = 0;
 
 always @(posedge acqClk) begin
-    acqStartAcquisitionToggle_m <= sysStartAcquisitionToggle;
-    acqStartAcquisitionToggle   <= acqStartAcquisitionToggle_m;
+    acqArmToggle_m <= sysArmToggle;
+    acqArmToggle   <= acqArmToggle_m;
+    acqArmToggle_d <= acqArmToggle;
     if (acqAcquisitionActive) begin
         dpram[acqWriteAddress] <= {dclk, drdy};
+        acqWriteAddress <= acqWriteAddress + 1;
     end
 
     case (acqState)
-    ST_STATE_IDLE: begin
-        acqWriteAddress <= 0;
-        if (acqStartAcquisitionToggle != acqStartAcquisitionMatch) begin
-            acqStartAcquisitionMatch <= acqStartAcquisitionToggle;
+    ST_IDLE: begin
+        preTriggerCounter <= PRE_TRIGGER_LOAD;
+        postTriggerCounter <= POST_TRIGGER_LOAD;
+        misalignCount <= MISALIGN_COUNT_LOAD;
+        if (acqArmToggle != acqArmToggle_d) begin
             acqAcquisitionActive <= 1;
-            acqState <= ST_STATE_AWAIT_LOW;
+            acqState <= ST_FILL;
         end
     end
-    ST_STATE_AWAIT_LOW: begin
-        if (drdy == 0) begin
-            acqState <= ST_STATE_AWAIT_RISING;
+    ST_FILL: begin
+        preTriggerCounter <= preTriggerCounter - 1;
+        if (preTriggerDone) begin
+            acqState <= ST_AWAIT_TRIGGER;
         end
     end
-    ST_STATE_AWAIT_RISING: begin
-        if (drdy != 0) begin
-            acqWriteAddress <= acqWriteAddress + 1;
-            acqState <= ST_STATE_ACTIVE;
+    ST_AWAIT_TRIGGER: begin
+        if ((drdy == {ADC_CHIP_COUNT{1'b0}})
+         || (drdy == {ADC_CHIP_COUNT{1'b1}})) begin
+            misalignCount <= MISALIGN_COUNT_LOAD;
+        end
+        else begin
+            misalignCount <= misalignCount - 1;
+        end
+        if (misaligned || (acqArmToggle != acqArmToggle_d)) begin
+            acqState <= ST_POST_TRIGGER;
         end
     end
-    ST_STATE_ACTIVE: begin
-        acqWriteAddress <= acqWriteAddress + 1;
-        if (acqWriteAddress == {ADDRESS_WIDTH{1'b1}}) begin
+    ST_POST_TRIGGER: begin
+        postTriggerCounter <= postTriggerCounter - 1;
+        if (postTriggerDone) begin
             acqAcquisitionActive <= 0;
-            acqState <= ST_STATE_IDLE;
+            acqState <= ST_IDLE;
         end
     end
     default: ;
@@ -137,7 +164,8 @@ end
 
 // Some values in acquisition clock domain, but races unimportant
 assign sysStatus = { acqAcquisitionActive,
-                     {32-1-(2*ADC_CHIP_COUNT){1'b0}},
+                     {32-1-ADDRESS_WIDTH-(2*ADC_CHIP_COUNT){1'b0}},
+                     acqWriteAddress,
                      sysReadQ };
 
 endmodule
